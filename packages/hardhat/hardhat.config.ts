e8c47cd001b08aec099b06d90fc685ff1c52128d
Nicolas Guasca <nicolas.guasca@gmail.com>
2021-11-05 16:19:24 +0100

    Test passing

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -25,6 +25,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false
+  },
   solidity: "0.8.4",
   networks: {
     hardhat: {
